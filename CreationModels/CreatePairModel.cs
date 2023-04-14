a6288d41cbbbf9eac601920a8cfc8981d7155e21
Dan4ef <70898270+Dan4ef@users.noreply.github.com>
2023-04-14 11:48:12 +0300

    Update CreatePairModel.cs

--- CreationModels/CreatePairModel.cs
+++ CreationModels/CreatePairModel.cs
@@ -2,8 +2,6 @@ namespace MyScheduler.CreationModels
 {
     public class CreatePairModel
     {
-        public int Id { get; set; }
-        
         public string DayOfWeek { get; set; }
         public string Time { get; set; }
     }
