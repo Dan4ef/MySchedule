455a39813a430f6736b601cf85cbb33056769403
Dan4ef <70898270+Dan4ef@users.noreply.github.com>
2023-04-14 11:21:55 +0300

    Update Pair.cs

--- Models/Pair.cs
+++ Models/Pair.cs
@@ -7,12 +7,12 @@
         public string DayOfWeek { get; set; }
         public string Time { get; set; }
         //
-        public int SubjectId { get; set; }
-        public Subject Subject { get; set; }
+        public int? SubjectId { get; set; }
+        public Subject? Subject { get; set; }
 
         //
-        public int ScheduleId { get; set; }
-        public Schedule Schedule { get; set; }
+        public int? ScheduleId { get; set; }
+        public Schedule? Schedule { get; set; }
         //
     }
 }
