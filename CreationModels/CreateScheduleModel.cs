3964250a93132fbd48a2df1a214d7d3e4e8ea76d
Dan4ef <70898270+Dan4ef@users.noreply.github.com>
2023-04-14 11:49:13 +0300

    Update CreateScheduleModel.cs

--- CreationModels/CreateScheduleModel.cs
+++ CreationModels/CreateScheduleModel.cs
@@ -2,8 +2,6 @@ namespace MyScheduler.CreationModels
 {
     public class CreateScheduleModel
     {
-        public int Id { get; set; }
-        
         public string Token { get; set; }
     }
 }
