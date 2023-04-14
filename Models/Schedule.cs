09f8e6c1d8fe3c3386f174b822810ec5e96db409
Dan4ef <70898270+Dan4ef@users.noreply.github.com>
2023-04-14 11:28:17 +0300

    Update Schedule.cs

--- Models/Schedule.cs
+++ Models/Schedule.cs
@@ -6,8 +6,8 @@
         
         public string Token { get; set; }
 
-        public int GroupId { get; set; }//посилаємось на GroupId - записане в полі id об'єкту класу Group
-        public Group Group { get; set; }
+        public int? GroupId { get; set; }//посилаємось на GroupId - записане в полі id об'єкту класу Group
+        public Group? Group { get; set; }
         
         //навігаційна властивість - список пар, в одному розкладі багато пар - один до багатьох
         public ICollection<Pair> scheduleItems { get; set; }
