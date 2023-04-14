3d6dc4186fab7b9825d6a5b50e66a99369cf6c81
Dan4ef <70898270+Dan4ef@users.noreply.github.com>
2023-04-14 11:30:01 +0300

    Create CreateSubjectModel.cs

---
+++ CreationModels/CreateSubjectModel.cs
@@ -0,0 +1,9 @@
+namespace MyScheduler.CreationModels
+{
+    public class CreateSubjectModel
+    {
+        public int  Id { get; set; } 
+        public string Name { get; set; }
+        public string Place { get; set; }
+    }
+}
